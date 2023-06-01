bffcc7d44194e776476a779cb7d83eba368d0552
Calvin Farrellino Kurniawan <calvin.kurniawan004@binus.ac.id>
2023-06-01 22:15:50 +0700

    Add Scroll Padding

--- resources/views/partials/app-footer.blade.php
+++ resources/views/partials/app-footer.blade.php
@@ -1,16 +1,16 @@
-<div class="container">
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top bg-dark ">
-        <div class="col-md-4 d-flex align-items-center">
-            <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-                <svg class="bi" width="30" height="24">
-                    <use xlink:href="#bootstrap" />
-                </svg>
-            </a>
-            <span class="mb-3 mb-md-0 text-white-50">&copy; 2023 Company, Inc</span>
-        </div>
-
-        <p class="float-end mb-1">
-            <a class="fw-bold back-to-top" href="#">Back to top &uarr;</a>
-        </p>
-    </footer>
+<div class="container mt-auto">
+    <div class="container">
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top ">
+            <div class="col-md-4 d-flex align-items-center">
+                <a href="https://getbootstrap.com/" target="_blank" class="mb-3 me-2 mb-md-0 text-decoration-none lh-1">
+                    <i class="fa-brands fa-bootstrap" style="color: rgb(0, 0, 0);"></i>
+                </a>
+                <span class="mb-3 mb-md-0 text-black">&copy; 2023 IndoJuni, Inc</span>
+            </div>
+            <p class="float-end mb-1">
+                <a class="fw-bold back-to-top text-black" href="#" style="text-decoration: none">Back to top
+                    &uarr;</a>
+            </p>
+        </footer>
+    </div>
 </div>
