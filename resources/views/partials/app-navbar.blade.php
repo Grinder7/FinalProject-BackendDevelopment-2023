a89a4cbd92d648e27c7b4194a4c6da2bc8bc10b3
Calvin Farrellino Kurniawan <calvin.kurniawan004@binus.ac.id>
2023-06-01 19:24:39 +0700

    Fixed Landing Page

--- resources/views/partials/app-navbar.blade.php
+++ resources/views/partials/app-navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="container">
     <header
-        class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 fixed-top px-3">
         <div class="col-md-3 mb-2 mb-md-0">
             <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
                 <svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap">
