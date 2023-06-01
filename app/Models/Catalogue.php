3e0de80ce9a80acc49550d27c920b4fb212324d8
Calvin Farrellino Kurniawan <calvin.kurniawan004@binus.ac.id>
2023-06-01 22:36:49 +0700

    Create Catalogue Model

---
+++ app/Models/Catalogue.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Catalogue extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'price',
+        'img',
+    ];
+    protected $primaryKey = "uuid";
+    protected $guarded = [
+        'uuid'
+    ];
+    public $timestamps = false;
+
+    public function cover(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($img) => config('catalogue.img_dir') . '/' . $img
+        );
+    }
+}
