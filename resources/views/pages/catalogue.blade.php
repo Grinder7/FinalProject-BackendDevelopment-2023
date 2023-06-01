3c7d996475a260e866865efe36c38ccd821ed5e0
Calvin Farrellino Kurniawan <calvin.kurniawan004@binus.ac.id>
2023-06-01 23:12:11 +0700

    Added default img

--- resources/views/pages/catalogue.blade.php
+++ resources/views/pages/catalogue.blade.php
@@ -3,14 +3,12 @@
     <section class="py-5 text-center container">
         <div class="row py-lg-5">
             <div class="col-lg-6 col-md-8 mx-auto">
-                <h1 class="fw-light">Album example</h1>
-                <p class="lead text-body-secondary">Something short and leading about the collection below—its contents, the
-                    creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.
+                <h1 class="fw-light">Katalog</h1>
+                <p class="lead text-body-secondary">Temukan berbagai produk berkualitas dengan harga terjangkau di toko kami.
+                    Kami berkomitmen untuk memberikan nilai tambah kepada pelanggan dengan penawaran spesial dan diskon
+                    menarik setiap minggunya.
                 </p>
-                <p>
-                    <a href="#" class="btn btn-primary my-2">Main call to action</a>
-                    <a href="#" class="btn btn-secondary my-2">Secondary action</a>
-                </p>
+
             </div>
         </div>
     </section>
