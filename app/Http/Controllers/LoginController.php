1b92f4adbe390af601a8691f9021d7a57f455c8b
Calvin Farrellino Kurniawan <calvin.kurniawan004@binus.ac.id>
2023-06-02 16:14:25 +0700

    Created User Repository

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Modules\User\UserService;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
+    public UserService $userService;
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
     public function index()
     {
         return view('pages.auth.login');
